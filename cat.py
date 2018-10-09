a43060569aa78ec8cd8c3275ea2eb3c53fd5ff14
mhenman <mark.henman@gmail.com>
2018-10-08 19:05:59 -0500

    Added cat.py for compiling text data files into a single csv

---
+++ cat.py
@@ -0,0 +1,33 @@
+import csv
+import glob
+import sys
+
+files = glob.glob("*.txt")
+
+headers = []
+data = []
+
+for file in files:
+  with open(file, 'r') as infile:
+    l = {}
+    for line in infile:
+      fields = line.strip().split(':')
+      if len(fields) != 2:
+        continue
+      if fields[0] not in headers:
+        headers.append(fields[0])
+      l[fields[0]] = fields[1].strip()
+    data.append(l)
+      
+#data.sort(key=lambda k: k['Match'])
+
+filename = '2018_STL_cat.csv'
+if len(sys.argv) > 1:
+  filename = sys.argv[1]
+
+with open(filename, 'wb') as outfile:
+  w = csv.DictWriter(outfile, fieldnames=headers)
+  w.writeheader()
+  for line in data:
+    w.writerow(line)
+
