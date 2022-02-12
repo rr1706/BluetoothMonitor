dbbbe0c4416434df08e1fcdf757ab24232ba638c
jchrist1732 <jchrist1732@gmail.com>
2022-02-12 14:57:34 -0600

    Update cat.py
    Add newline to cat.py to avoid empty rows in csv

--- cat.py
+++ cat.py
@@ -25,7 +25,7 @@ filename = '2018_STL_cat.csv'
 if len(sys.argv) > 1:
   filename = sys.argv[1]
 
-with open(filename, 'w') as outfile:
+with open(filename, 'w', newline='') as outfile:
   w = csv.DictWriter(outfile, fieldnames=headers)
   w.writeheader()
   for line in data:
